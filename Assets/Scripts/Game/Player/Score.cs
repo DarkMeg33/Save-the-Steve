63371fbc2629d7455f4a1ca00324e1a274ced8e2
DarkMeg <foki33@mail.ru>
2022-02-04 22:45:01 +0200

    commit

--- Assets/Scripts/Game/Player/Score.cs
+++ Assets/Scripts/Game/Player/Score.cs
@@ -1,17 +1,23 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
+    [SerializeField] private Text _highestScoreInscription;
+
     private int _value;
     private int _highestScore;
     private Text _scoreText;
+
+    private bool _isRecord = false;
 
     public int Value => _value;
 
     private void Awake()
     {
         EventSystem.OnEnemyKilled.AddListener(IncreaseScore);
+        EventSystem.OnGameOver.AddListener(SetRecord);
 
         _value = 0;
         _highestScore = PlayerPrefs.GetInt("HighestScore");
@@ -23,9 +29,37 @@ public class Score : MonoBehaviour
         _value += points;
         _scoreText.text = "Score: " + _value;
 
-        if (_value > _highestScore)
+        if (!_isRecord)
+            CheckRecord(_value);
+    }
+
+    private void CheckRecord(int score)
+    {
+        if (score > _highestScore)
         {
-            PlayerPrefs.SetInt("HighestScore", _value);
+            _scoreText.color = Color.yellow;
+            _isRecord = true;
+
+            StartCoroutine(Congratulate());
         }
+        Debug.Log("Score: " + score);
+        Debug.Log("Record: " + _highestScore);
+    }
+
+    private void SetRecord()
+    {
+        PlayerPrefs.SetInt("HighestScore", _value);
+    }
+
+    private IEnumerator Congratulate()
+    {
+       _highestScoreInscription.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(5);
+
+        _highestScoreInscription.gameObject.SetActive(false);
+
+        Destroy(_highestScoreInscription);
+        StopCoroutine(Congratulate());
     }
 }
